03b10dfc42ed1cea0539d8f16d2aa79335e4ad5b
Tolomaus <niek.bartholomeus@gmail.com>
2011-12-16 22:17:16 +0100

    change migration to support pg

--- db/migrate/20111101120456_change_status_severity_pcrs.rb
+++ db/migrate/20111101120456_change_status_severity_pcrs.rb
@@ -5,7 +5,7 @@ class ChangeStatusSeverityPcrs < ActiveRecord::Migration
       t.rename :severity, :severity_id
       t.remove :status_id
       t.column :status_id, :integer
-      t.remove :severity_id, :integer
+      t.remove :severity_id
       t.column :severity_id, :integer
     end
   end
